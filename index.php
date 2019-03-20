52c08c7de1e2bc32509dd648c40557c93db61c33
llagy <mrivera.scorpio@gmail.com>
2019-03-20 15:37:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
    
     
 <head>
-	<!--script src="https://cdnjs.cloudflare.com/ajax/libs/ClientJS/0.1.11/client.min.js"></script>-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/ClientJS/0.1.11/client.min.js"></script>
 	<!--script  src="client.min.js"></script>-->
 		
 </head>
@@ -10,46 +10,11 @@
 
     
 <body>
-<?php
-require_once('Mobile_Detect.php');
-
-$mobile = new Mobile_Detect();
-
-if($mobile->isAndroidOS()){
-	$DEVICE = 'android';
-	
-}
-else if($mobile->isiOS()){
-	$DEVICE = 'iphone';
-}
-else{
-	$DEVICE = 'Dispositivo desconocido';
-}
-//$DEVICE = 'iphone';
-//echo  $DEVICE;
-if($DEVICE=='android')
-    
-    echo '<H1>El disposito es '.$DEVICE.'</H1><br/><img src=android.jpg alt=android>';
-else if($DEVICE=='android')
-    echo '<H1>El disposito es '.$DEVICE.'</H1><br/><img src=iphone.jpg alt=android>';
-else
 
 
-$ch = curl_init();
-//curl_setopt($ch, CURLOPT_URL, "https://api.github.com/users/KrunalLathiya");
-curl_setopt($ch, CURLOPT_URL, "http://35.226.148.54/grupo-paas");
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('device: '.$DEVICE.''));
-
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$res = curl_exec($ch);
-//echo $res;
-curl_close($ch);
-
-
-?>
 <script>
 
-/* function descDevice(){
+ //function descDevice(){
 
 	var client = new ClientJS(); // Create A New Client Object
 
@@ -72,33 +37,28 @@ curl_close($ch);
 	}
 	document.write("<br>"+divice);
 	console.log("fin de logica" );
-	return divice;
- }	*/
+	//return divice;
+ 	
+ 
+ divice='iphone';
 	//------------------------------------------------
 
-
-  //url='http://35.226.148.54/grupo-paas';
-  
-//------------------------------------------------
-/* fetch('http://35.226.148.54/grupo-paas', {
-  headers: new Headers({
-    'device': 'iphone',
+ 
+fetch('http://35.226.148.54/grupo-paas', { 
+    method: 'GET',
+    headers: new Headers({
+    'device': divice,
+	 
 	
   })
 })
-.then(response => response.json())
+.then( response => response.json())
 .then(data => {
   console.log(data)
 })
-.catch(error => console.error(error))*/
+.catch(error => console.error(error))
 
 
-
-
-	//----------------------------
-	
-	
-//}
  </script>
  
  
